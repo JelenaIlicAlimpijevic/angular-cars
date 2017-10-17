4d13d825dbb025c14a7eec53e340461fb34d8b4e
JelenaIlicAlimpijevic <ma1jelena@yahoo.com>
2017-10-17 18:51:07 +0200

    prvi zadatak

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CarsComponent} from './components/cars/cars.component';
+
+const appRoutes: Routes = [
+  {path: 'cars', component: CarsComponent}
+  
+  
+];
+
+@NgModule({
+  imports:[
+    RouterModule.forRoot(appRoutes)
+  ], 
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {}
